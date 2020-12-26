103455a132f016a689235a85fdaeec21fa0f8121
CAI <lanfearcai@icloud.com>
2020-12-26 12:13:58 +0800

    specific class L2 norm distribution

--- cifar/cal_distribution.py
+++ cifar/cal_distribution.py
@@ -1,6 +1,7 @@
 import argparse
 import os
 import time
+from numpy import linalg as LA
 
 from utee import misc
 import torch
@@ -8,23 +9,26 @@ import torch.nn.functional as F
 import torch.optim as optim
 from torch.autograd import Variable
 
-import dataset
+# import dataset
+from torchvision import datasets, transforms
+import matplotlib.pyplot as plt
 
 from IPython import embed
 
 from imagenet import alexnet
+plt.switch_backend('agg')
 
 
 
 parser = argparse.ArgumentParser(description='PyTorch CIFAR-X Example')
-parser.add_argument('--type', default='cifar10', help='cifar10|cifar100')
+parser.add_argument('--type', default='cifar100', help='cifar10|cifar100')
 parser.add_argument('--channel', type=int, default=128, help='first conv channel (default: 32)')
 parser.add_argument('--wd', type=float, default=0.00, help='weight decay')
 parser.add_argument('--batch_size', type=int, default=200, help='input batch size for training (default: 64)')
 parser.add_argument('--epochs', type=int, default=150, help='number of epochs to train (default: 10)')
 parser.add_argument('--lr', type=float, default=0.001, help='learning rate (default: 1e-3)')
 parser.add_argument('--gpu', default=None, help='index of gpus to use')
-parser.add_argument('--ngpu', type=int, default=2, help='number of gpus to use')
+parser.add_argument('--ngpu', type=int, default=1, help='number of gpus to use')
 parser.add_argument('--seed', type=int, default=117, help='random seed (default: 1)')
 parser.add_argument('--log_interval', type=int, default=100,  help='how many batches to wait before logging training status')
 parser.add_argument('--test_interval', type=int, default=5,  help='how many epochs to wait before another test')
@@ -55,14 +59,19 @@ if args.cuda:
 # data loader and model
 assert args.type in ['cifar10', 'cifar100'], args.type
 if args.type == 'cifar10':
-    train_loader, test_loader = dataset.get10(batch_size=args.batch_size, num_workers=1)
-    model = alexnet.alexnet(100, model_root='log')
+    # train_loader, test_loader = dataset.get10(batch_size=args.batch_size, num_workers=1)
+    model = alexnet.alexnet()
+    model.load_state_dict(torch.load('log/default/latest.pth'))
 else:
-    train_loader, test_loader = dataset.get100(batch_size=args.batch_size, num_workers=1)
-    model = alexnet.alexnet(100, model_root='log')
+    # train_loader, test_loader = dataset.get100(batch_size=args.batch_size, num_workers=1)
+    model = alexnet.alexnet()
+    model.load_state_dict(torch.load('log/default/latest.pth'))
+
+#to GPU
 model = torch.nn.DataParallel(model, device_ids= range(args.ngpu))
 if args.cuda:
     model.cuda()
+
 
 # optimizer
 optimizer = optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.wd)
@@ -70,63 +79,79 @@ decreasing_lr = list(map(int, args.decreasing_lr.split(',')))
 print('decreasing_lr: ' + str(decreasing_lr))
 best_acc, old_file = 0, None
 t_begin = time.time()
+
+
+norm_train=[]
+norm_test=[]
+
+def get_indices(dataset,class_name):
+    indices =  []
+    for i in range(len(dataset.targets)):
+        if dataset.targets[i] == class_name:
+            indices.append(i)
+    return indices
+
+dataset = datasets.CIFAR100(
+                root='/tmp/public_dataset/pytorch', train=True, download=True,
+                transform=transforms.Compose([
+                transforms.ToTensor(),
+                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                ]))
+idx = get_indices(dataset, 1)
+train_loader = torch.utils.data.DataLoader(dataset,batch_size=args.batch_size, sampler = torch.utils.data.sampler.SubsetRandomSampler(idx))
+
+dataset = datasets.CIFAR100(
+                root='/tmp/public_dataset/pytorch', train=False, download=True,
+                transform=transforms.Compose([
+                transforms.ToTensor(),
+                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                ]))
+idx = get_indices(dataset, 1)
+test_loader = torch.utils.data.DataLoader(dataset,batch_size=args.batch_size, sampler = torch.utils.data.sampler.SubsetRandomSampler(idx))
+
 try:
-    # ready to go
-    for epoch in range(args.epochs):
-        model.train()
-        if epoch in decreasing_lr:
-            optimizer.param_groups[0]['lr'] *= 0.1
-        for batch_idx, (data, target) in enumerate(train_loader):
-            indx_target = target.clone()
-            if args.cuda:
-                data, target = data.cuda(), target.cuda()
-            data, target = Variable(data), Variable(target)
+    # ready to test
+    model.eval()
+    test_loss = 0
+    correct = 0
 
-            optimizer.zero_grad()
-            output = model(data)
-            loss = F.cross_entropy(output, target)
-            loss.backward()
-            optimizer.step()
+    i = 0
+    for data, target in train_loader:
+        indx_target = target.clone()
+        if args.cuda:
+            data, target = data.cuda(), target.cuda()
+        data, target = Variable(data, volatile=True), Variable(target)
+        output = model(data)
+        norm_train.append(LA.norm(output.cpu().detach().numpy()))#norm_train
+        test_loss += F.cross_entropy(output, target).data
+        pred = output.data.max(1)[1]  # get the index of the max log-probability
+        correct += pred.cpu().eq(indx_target).sum()
+        i += 1
+        if i >= 500:
+            break
 
-            if batch_idx % args.log_interval == 0 and batch_idx > 0:
-                pred = output.data.max(1)[1]  # get the index of the max log-probability
-                correct = pred.cpu().eq(indx_target).sum()
-                acc = correct * 1.0 / len(data)
-                print('Train Epoch: {} [{}/{}] Loss: {:.6f} Acc: {:.4f} lr: {:.2e}'.format(
-                    epoch, batch_idx * len(data), len(train_loader.dataset),
-                    loss.data, acc, optimizer.param_groups[0]['lr']))
+    j = 0
+    for data, target in test_loader:
+        indx_target = target.clone()
+        if args.cuda:
+            data, target = data.cuda(), target.cuda()
+        data, target = Variable(data, volatile=True), Variable(target)
+        output = model(data)
+        norm_test.append(LA.norm(output.cpu().detach().numpy()))#norm_test
+        test_loss += F.cross_entropy(output, target).data
+        pred = output.data.max(1)[1]  # get the index of the max log-probability
+        correct += pred.cpu().eq(indx_target).sum()
+        j += 1
+        if j >= 500:
+            break
+    
+    # print(norm_test)
+    # print(norm_train)
+    plt.hist([norm_train,norm_test],bins=40, alpha=0.8)
+    plt.legend()
+    plt.savefig('grad_hist')
+    
 
-        elapse_time = time.time() - t_begin
-        speed_epoch = elapse_time / (epoch + 1)
-        speed_batch = speed_epoch / len(train_loader)
-        eta = speed_epoch * args.epochs - elapse_time
-        print("Elapsed {:.2f}s, {:.2f} s/epoch, {:.2f} s/batch, ets {:.2f}s".format(
-            elapse_time, speed_epoch, speed_batch, eta))
-        misc.model_snapshot(model, os.path.join(args.logdir, 'latest.pth'))
-
-        if epoch % args.test_interval == 0:
-            model.eval()
-            test_loss = 0
-            correct = 0
-            for data, target in test_loader:
-                indx_target = target.clone()
-                if args.cuda:
-                    data, target = data.cuda(), target.cuda()
-                data, target = Variable(data, volatile=True), Variable(target)
-                output = model(data)
-                test_loss += F.cross_entropy(output, target).data
-                pred = output.data.max(1)[1]  # get the index of the max log-probability
-                correct += pred.cpu().eq(indx_target).sum()
-
-            test_loss = test_loss / len(test_loader) # average over number of mini-batch
-            acc = 100. * correct / len(test_loader.dataset)
-            print('\tTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)'.format(
-                test_loss, correct, len(test_loader.dataset), acc))
-            if acc > best_acc:
-                new_file = os.path.join(args.logdir, 'best-{}.pth'.format(epoch))
-                misc.model_snapshot(model, new_file, old_file=old_file, verbose=True)
-                best_acc = acc
-                old_file = new_file
 except Exception as e:
     import traceback
     traceback.print_exc()
